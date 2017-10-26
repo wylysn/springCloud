844d96f0771fea4ec715f17f4465c3885917cebc
wangyilu <wylysn@gmail.com>
2017-10-26 15:27:12 +0800

    更新

--- config-client/src/main/java/com/purang/config_client/ConfigClientApplication.java
+++ config-client/src/main/java/com/purang/config_client/ConfigClientApplication.java
@@ -3,11 +3,23 @@ package com.purang.config_client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * 配置更新（/bus/refresh）必须要,还需要配置management.security.enabled=false
+ * 安装rabbitmq（mac）：brew install rabbitmq
+ * 启动: rabbitmq-server –detached
+ * 关闭:rabbitmqctl stop
+ * mq管理端口：15672，连接端口：5672
+ * @author imac
+ *
+ */
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
+@RefreshScope	
 public class ConfigClientApplication 
 {
     public static void main( String[] args )
